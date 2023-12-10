5f4a62a91b010106d1486185f5b8b0aa51749cec
sh1okoh <kooooo.0719task@gmail.com>
2023-12-10 17:53:41 +0900

    feat

--- local-storage-cross-domain-communication/sample1/src/App.tsx
+++ local-storage-cross-domain-communication/sample1/src/App.tsx
@@ -1,33 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useState('')
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.target.value)
+  }
+
+  const handleOnClick = () => {
+    localStorage.setItem('text', text)
+  }
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <textarea id="textarea" rows={10} cols={50} onChange={handleOnChange} value={text}>
+        </textarea>
+        <button onClick={handleOnClick}>
+          Save
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
